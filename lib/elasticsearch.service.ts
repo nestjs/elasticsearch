6f21c9f8d4fd66147ce5199610526726268d166d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-12-09 16:43:21 +0100

    chore() publish 0.2.0 release

--- lib/elasticsearch.service.ts
+++ lib/elasticsearch.service.ts
@@ -1,112 +1,142 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { BulkIndexDocumentsParams, ClearScrollParams, Client, CountParams, CreateDocumentParams, DeleteDocumentByQueryParams, DeleteDocumentParams, DeleteScriptParams, ExistsParams, FieldStatsParams, GetParams, IndexDocumentParams, InfoParams, PingParams, SearchParams, UpdateDocumentByQueryParams, UpdateDocumentParams } from 'elasticsearch';
+import {
+  BulkIndexDocumentsParams,
+  ClearScrollParams,
+  Client,
+  CountParams,
+  CountResponse,
+  CreateDocumentParams,
+  CreateDocumentResponse,
+  DeleteDocumentByQueryParams,
+  DeleteDocumentParams,
+  DeleteDocumentResponse,
+  DeleteScriptParams,
+  ExistsParams,
+  FieldStatsParams,
+  FieldStatsResponse,
+  GetParams,
+  GetResponse,
+  GetSourceParams,
+  IndexDocumentParams,
+  InfoParams,
+  PingParams,
+  SearchParams,
+  SearchResponse,
+  UpdateDocumentByQueryParams,
+  UpdateDocumentByQueryResponse,
+  UpdateDocumentParams
+} from 'elasticsearch';
 import { bindNodeCallback, Observable } from 'rxjs';
 import { ELASTICSEARCH_CLIENT } from './elasticsearch-client.provider';
 
 @Injectable()
 export class ElasticsearchService {
   constructor(
-    @Inject(ELASTICSEARCH_CLIENT) private readonly esClient: Client,
+    @Inject(ELASTICSEARCH_CLIENT) private readonly esClient: Client
   ) {}
 
   getClient(): Client {
     return this.esClient;
   }
 
+  getSource(params: GetSourceParams): Observable<any> {
+    return bindNodeCallback(this.bindClientContext(this.esClient.clearScroll))(
+      params
+    );
+  }
+
   clearScroll(params: ClearScrollParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.clearScroll),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.clearScroll))(
+      params
+    );
   }
 
   ping(params: PingParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.ping),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.ping))(params);
   }
 
-  search(params: SearchParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.search)
-    )(params); 
+  search<T = any>(params: SearchParams): Observable<SearchResponse<T>> {
+    return (bindNodeCallback(this.bindClientContext(this.esClient.search))(
+      params
+    ) as any) as Observable<SearchResponse<T>>;
   }
 
-  count(params: CountParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.count),
-    )(params);
+  count(params: CountParams): Observable<CountResponse> {
+    return (bindNodeCallback(this.bindClientContext(this.esClient.count))(
+      params
+    ) as any) as Observable<CountResponse>;
   }
 
-  create(params: CreateDocumentParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.create),
-    )(params);
+  create(params: CreateDocumentParams): Observable<CreateDocumentResponse> {
+    return (bindNodeCallback(this.bindClientContext(this.esClient.create))(
+      params
+    ) as any) as Observable<CreateDocumentResponse>;
   }
 
   update(params: UpdateDocumentParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.update),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.update))(
+      params
+    );
   }
 
-  updateByQuery(params: UpdateDocumentByQueryParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.updateByQuery),
-    )(params);
+  updateByQuery(
+    params: UpdateDocumentByQueryParams
+  ): Observable<UpdateDocumentByQueryResponse> {
+    return (bindNodeCallback(
+      this.bindClientContext(this.esClient.updateByQuery)
+    )(params) as any) as Observable<UpdateDocumentByQueryResponse>;
   }
 
-  delete(params: DeleteDocumentParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.delete),
-    )(params);
+  delete(params: DeleteDocumentParams): Observable<DeleteDocumentResponse> {
+    return (bindNodeCallback(this.bindClientContext(this.esClient.delete))(
+      params
+    ) as any) as Observable<DeleteDocumentResponse>;
   }
 
-  deleteByQuery(params: DeleteDocumentByQueryParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.deleteByQuery),
-    )(params);
+  deleteByQuery(
+    params: DeleteDocumentByQueryParams
+  ): Observable<DeleteDocumentByQueryParams> {
+    return (bindNodeCallback(
+      this.bindClientContext(this.esClient.deleteByQuery)
+    )(params) as any) as Observable<DeleteDocumentByQueryParams>;
   }
 
   deleteScript(params: DeleteScriptParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.deleteScript),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.deleteScript))(
+      params
+    );
   }
 
   exists(params: ExistsParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.exists),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.exists))(
+      params
+    );
   }
 
   bulk(params: BulkIndexDocumentsParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.bulk),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.bulk))(params);
   }
 
-  fieldStats(params: FieldStatsParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.fieldStats),
-    )(params);
+  fieldStats(params: FieldStatsParams): Observable<FieldStatsResponse> {
+    return (bindNodeCallback(this.bindClientContext(this.esClient.fieldStats))(
+      params
+    ) as any) as Observable<FieldStatsResponse>;
   }
 
-  get(params: GetParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.get),
-    )(params);
+  get<T = any>(params: GetParams): Observable<GetResponse<T>> {
+    return (bindNodeCallback(this.bindClientContext(this.esClient.get))(
+      params
+    ) as any) as Observable<GetResponse<T>>;
   }
 
   index<T>(params: IndexDocumentParams<T>): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.index),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.index))(
+      params
+    );
   }
 
   info(params: InfoParams): Observable<any> {
-    return bindNodeCallback(
-      this.bindClientContext(this.esClient.info),
-    )(params);
+    return bindNodeCallback(this.bindClientContext(this.esClient.info))(params);
   }
 
   close() {
